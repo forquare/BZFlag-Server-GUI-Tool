747825599a7a496bb9b86a382e44459b4f17f3db
Ben Lavery <ben.lavery@gmail.com>
2011-04-19 21:06:00 +0100

    Forgot build and dist - They can cause problems when merging repos.

---
+++ src/Main/Controller.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Main;
+import GUI.MainFrame;
+
+/**
+ *
+ * @author benlavery
+ * @version 110417
+ */
+public class Controller {
+
+    private MainFrame gui;
+
+    public Controller(MainFrame mf){
+        gui = mf;
+    }
+
+    public void launchServer(){
+        
+    }
+
+    public void killServer(){
+
+    }
+
+    public void importSettings(){
+
+    }
+
+    public void exportSettings(){
+
+    }
+
+}
