f9fe692399202126bbbe6bb18424982d56906758
Ben Lavery <ben.lavery@gmail.com>
2011-04-27 04:03:21 +0100

    Added maps and started to test out launch options

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -5,6 +5,10 @@
 
 package Main;
 import GUI.MainFrame;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,8 +22,8 @@ public class Controller {
     public Controller(){
         gui = new MainFrame();
 
-        //Temp add random map
-        gui.getCmbMaps().addItem("BOOM");
+        //Set maps for GUI
+        setMaps();
 
         gui.setController(this);
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -29,9 +33,48 @@ public class Controller {
         });
     }
 
+    private void setMaps(){
+        String OS = System.getProperty("os.name");
+        String homeDir = System.getProperty("user.home");
+        File mapsDir;
+        String mapsPath;
+
+        if(OS.toLowerCase().contains("window")){
+            mapsPath = homeDir + "\\My Documents\\My BZFlag Files\\maps";
+        }else{
+            mapsPath = homeDir + "/.maps";
+        }
+
+        mapsDir = new File(mapsPath);
+        if(!mapsDir.exists()){
+            gui.printError("There is no maps folder in " + mapsPath);
+            System.exit(0);
+        }
+        String[] maps = mapsDir.list();
+        if(maps.length == 0){
+            gui.printError("There are no suitable maps in " + mapsPath);
+            System.exit(0);
+        }
+        for(int i = 0; i < maps.length; i++){
+            if(maps[i].toLowerCase().contains("bzw") || maps[i].toLowerCase().contains("map")){
+                gui.getCmbMaps().addItem(maps[i]);
+            }
+        }
+    }
+
     public void launchServer(){
-        exportSettings();
-        //Run server
+        if(System.getProperty("os.name").toLowerCase().contains("window")){
+            exportSettings(System.getenv("windir") + "\\TEMP\\bzflag-server-gui-temp-config");
+        }else{
+            exportSettings("/tmp/bzflag-server-gui-temp-config");
+            try {
+                Process p = Runtime.getRuntime().exec("/usr/games/bzfs -conf /tmp/bzflag-server-gui-temp-config");
+            } catch (IOException ex) {
+                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        
     }
 
     public void killServer(){
@@ -42,8 +85,8 @@ public class Controller {
 
     }
 
-    public void exportSettings(){
-        Exporter exp = new Exporter(gui, "/tmp/myTest.txt");
+    public void exportSettings(String path){
+        Exporter exp = new Exporter(gui, path);
     }
 
 }
